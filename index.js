75f249a67196ba85bc731e96d4e35a6590923c4a
Erum azeemi qaimkhani <160758395+erumQaimkhani@users.noreply.github.com>
2024-08-21 23:11:58 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+import { input } from "@inquirer/prompts";
+let answer = await input({
+    message: 'Enter your name?',
+});
+let answer2 = await input({
+    message: 'Enter your father name?',
+});
+console.log(answer, answer2);
